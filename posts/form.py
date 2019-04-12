2ebbf100d405e44f9f3f06f8f7319638b901eadf
kshm2483 <kimsuhan2483@gmail.com>
2019-04-12 06:24:59 +0000

    04_upload multiple images

--- posts/form.py
+++ posts/form.py
@@ -1,11 +1,19 @@
 from django import forms
-from .models import Post
+from .models import Post, Image
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['content', 'image',]
+        fields = ['content',]
         widgets = {'content': forms.TextInput(attrs={
                                                     'placeholder': '내용을 입력하세요.',
                                                     'class': 'content',}),
+        }
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['file',]
+        widgets = {'file': forms.FileInput(attrs={
+                                                'multiple':True, }),
         }
